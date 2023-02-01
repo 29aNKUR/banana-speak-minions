cb45f5d57e7e9be6bf647062aa48008d1dba84f5
Ankur Semle <semleankur@gmail.com>
2023-02-01 20:51:36 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output")
+
+
 
 function clickHandler(){
-    console.log("clicked!");
-    console.log("input",txtInput.value);
+    // console.log("clicked!");
+    // console.log("input",txtInput.value);
+    outputDiv.innerText = "asdfsdaf " + txtInput.value;
 };
 
 btnTranslate.addEventListener("click",clickHandler);
